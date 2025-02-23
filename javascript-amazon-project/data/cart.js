176e510b40b8c10380a5496f43c47649017f7c73
hrushithar <hrushithar27@gmail.com>
2025-02-23 11:17:10 +0530

    converted code to promises in cart.js and products.js

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -73,3 +73,13 @@ export function addToCart(productId) {
 
     saveToStorage();
   }
+
+  export function loadCart(fun) {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', () => {
+      console.log('load cart');
+      fun();
+    });
+    xhr.open('GET', 'https://supersimplebackend.dev/cart');
+    xhr.send();
+  }
